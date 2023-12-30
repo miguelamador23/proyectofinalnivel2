99857826bf4fb87078357e80e4d8776cfcfdb7eb
¨miguelamador23¨ <miguelamador23@outlook.com>
2023-12-29 22:00:34 -0600

    login con usuarios

--- index.php
+++ index.php
@@ -1,21 +1,46 @@
 <?php
 require_once 'Models/Database.php';
+session_start();
 
-$email = isset($_POST['email']) ? $_POST['email'] : '';
-$password = isset($_POST['password']) ? $_POST['password'] : '';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-$stmt = $mysqli->prepare("SELECT * FROM login WHERE Correo = ? AND Contraseña = ?");
-$stmt->bind_param("ss", $email, $password);
-$stmt->execute();
-$result = $stmt->get_result();
+    $sql = "SELECT id FROM usuarios WHERE Correo = ? AND Contraseña = ?";
+    $stmt = $mysqli->prepare($sql);
 
-if ($result && $result->num_rows > 0) {
-    echo "Inicio de sesión exitoso";
-} elseif ($result) {
-} else {
-    echo "Credenciales incorrectas";
+    if (!$stmt) {
+        echo "Error en la preparación de la consulta: " . $mysqli->error;
+        exit();
+    }
+
+    $stmt->bind_param("ss", $email, $password);
+    $result = $stmt->execute();
+
+    if ($result) {
+        $stmt->bind_result($el_id_del_usuario);
+        $stmt->fetch();
+        $stmt->close();
+
+        if ($email === 'admin@admin' && $password === 'admin') {
+            $_SESSION['user_id'] = $el_id_del_usuario;
+            header('Location: View/DashboardAdmin.php');
+            exit();
+        } elseif ($email === 'maestro@maestro' && $password === 'maestro') {
+            $_SESSION['user_id'] = $el_id_del_usuario;
+            header("Location: View/DashboardMaestro.php");
+            exit();
+        } else {
+            echo "Error de usuario incorrecto. Por favor, verifica tus credenciales.";
+            exit();
+        }
+    } else {
+        echo "Error al ejecutar la consulta: " . $stmt->error;
+        exit();
+    }
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -26,11 +51,11 @@ if ($result && $result->num_rows > 0) {
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <style>
         body {
-            background-color: #fff5d2;
+            background-color: rgba(255, 245, 210, 255);
         }
-        
-        section{
-            margin-top: 80px;
+
+        .container {
+            position: relative;
         }
 
         .image {
@@ -46,46 +71,27 @@ if ($result && $result->num_rows > 0) {
 </head>
 
 <body>
-    <div>
-        <img src="assets/logo.jpg" alt="" class="image">
-    </div>
-    <section class="bg-hash-fff5d2 dark:bg-gray-900">
-        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                        Bienvenido, ingresa con tu cuenta
-                    </h1>
-                    <form class="space-y-4 md:space-y-6" action="#">
-                        <div>
-                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Correo electrónico:</label>
-                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email" required="">
-                        </div>
-                        <div>
-                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contraseña:</label>
-                            <input type="password" name="password" id="password" placeholder="Password" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
-                        </div>
-                        <div class="flex items-center justify-between">
-                            <div class="flex items-start">
-                                <div class="flex items-center h-5">
-                                    <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800">
-                                </div>
-                                <div class="ml-3 text-sm">
-                                    <label for="remember" class="text-gray-500 dark:text-gray-300">Recuérdame</label>
-                                </div>
-                            </div>
-                            <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">¿Olvidaste tu contraseña?</a>
-                        </div>
-                        <button type="submit" class="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Iniciar sesión</button>
-                        <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                            Don’t have an account yet? <a href="#" class="font-medium text-Blue-600 hover:underline dark:text-primary-500">Sign up</a>
-                        </p>
-                    </form>
-                </div>
+    <img src="../assets/logo.jpg" alt="" class="image">
+    <div class="container">
+        <div class="flex justify-center items-center h-screen">
+            <div class="bg-white shadow-lg rounded-lg p-8">
+                <h2 class="text-2xl mb-4">Bienvenido ingresa con tu cuenta</h2>
+                <form id="loginForm" method="post" action="index.php">
+                    <div class="mb-4">
+                        <input type="email"  name="email" id="email" class="border border-gray-300 rounded-md p-2 w-full"
+                            placeholder="Correo" required />
+                    </div>
+                    <div class="mb-4">
+                        <input type="password" name="password" id="password" class="border border-gray-300 rounded-md p-2 w-full"
+                            placeholder="Contraseña" required />
+                    </div>
+                    <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-md">Iniciar sesión</button>
+                </form>
             </div>
         </div>
-    </section>
+    </div>
 
+   
 </body>
 
 </html>
